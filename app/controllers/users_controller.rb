40308b9f4e524d4576768cd939cb3a37076858fe
madofuki <nkutl9up@gmail.com>
2021-04-29 17:43:18 +0900

    add logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    debugger
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       # TODO: トップページにリダイレクトする
       render 'new'
